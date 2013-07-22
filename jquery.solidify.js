04c7eee8fdfac3c646bdce48540f3c112a79674c
Theophane RUPIN <theophane.rupin@gmail.com>
2013-07-22 11:17:13 +0200

    add bower manifest + jquery.solidify.js

---
+++ jquery.solidify.js
@@ -0,0 +1,36 @@
+/**
+ * User: rupin_t
+ * Date: 7/22/13
+ * Time: 11:00 AM
+ */
+
+(function ($) {
+
+    $.solidify = (function (options) {
+        options = options || {};
+        options.crawlableAgent = options.crawlableAgent || 'phantom.js';
+
+        var isCrawlable = navigator.userAgent == options.crawlableAgent;
+
+        /**
+         * Helper to permit compatibility with solidify.
+         */
+        Handlebars.registerHelper('solidify', function (url, options) {
+            return '{{#solidify "' + url + '"}}' + options.fn(this) + '{{/solidify}}';
+        });
+
+        return function (rawTemplate) {
+            if (!isCrawlable) {
+                var inR = /\{\{#solidify .*\}\}/, outR = /\{\{\/solidify\}\}/;
+                while (rawTemplate.match(inR) || rawTemplate.match(outR))
+                    rawTemplate = rawTemplate.replace(inR, '').replace(outR, '');
+                while (rawTemplate.indexOf('{ { {') !== -1 || rawTemplate.indexOf('} } }') !== -1)
+                    rawTemplate = rawTemplate.replace('{ { {', '{{{').replace('} } }', '}}}');
+                while (rawTemplate.indexOf('{ {') !== -1 || rawTemplate.indexOf('} }') !== -1)
+                    rawTemplate = rawTemplate.replace('{ {', '{{').replace('} }', '}}');
+            }
+            return rawTemplate;
+        };
+    })();
+
+})(jQuery);
