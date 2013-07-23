52aa77642e6baabcc69f8432124343eea6608700
Theophane RUPIN <theophane.rupin@gmail.com>
2013-07-23 18:29:39 +0200

    full reimplementation.

--- jquery.solidify.js
+++ jquery.solidify.js
@@ -7,27 +7,20 @@
 (function ($) {
     var isCrawlable = navigator.userAgent == 'phantom.js';
 
-    $.solidify = (function () {
+    Handlebars.registerHelper('solidify', function (url, options) {
+        return '{{#solidify "' + url + '"}}' + options.fn(this) + '{{/solidify}}';
+    });
 
-        /**
-         * Helper to permit compatibility with solidify.
-         */
-        Handlebars.registerHelper('solidify', function (url, options) {
-            return '{{#solidify "' + url + '"}}' + options.fn(this) + '{{/solidify}}';
-        });
-
-        return function (rawTemplate) {
-            if (!isCrawlable) {
-                var inR = /\{\{#solidify .*\}\}/, outR = /\{\{\/solidify\}\}/;
-                while (rawTemplate.match(inR) || rawTemplate.match(outR))
-                    rawTemplate = rawTemplate.replace(inR, '').replace(outR, '');
-                while (rawTemplate.indexOf('{ { {') !== -1 || rawTemplate.indexOf('} } }') !== -1)
-                    rawTemplate = rawTemplate.replace('{ { {', '{{{').replace('} } }', '}}}');
-                while (rawTemplate.indexOf('{ {') !== -1 || rawTemplate.indexOf('} }') !== -1)
-                    rawTemplate = rawTemplate.replace('{ {', '{{').replace('} }', '}}');
-            }
-            return rawTemplate;
-        };
-    })();
-
-})(Backbone ? Backbone.$ : jQuery);
+    $.solidify = function (rawTemplate) {
+        if (!isCrawlable) {
+            var inR = /\{\{#solidify .*\}\}/, outR = /\{\{\/solidify\}\}/;
+            while (rawTemplate.match(inR) || rawTemplate.match(outR))
+                rawTemplate = rawTemplate.replace(inR, '').replace(outR, '');
+            while (rawTemplate.indexOf('{ { {') !== -1 || rawTemplate.indexOf('} } }') !== -1)
+                rawTemplate = rawTemplate.replace('{ { {', '{{{').replace('} } }', '}}}');
+            while (rawTemplate.indexOf('{ {') !== -1 || rawTemplate.indexOf('} }') !== -1)
+                rawTemplate = rawTemplate.replace('{ {', '{{').replace('} }', '}}');
+        }
+        return rawTemplate;
+    };
+})(jQuery);
