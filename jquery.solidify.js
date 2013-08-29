6261dd286f053a5fe0362831a449bd3b8ca67009
Theophane RUPIN <theophane.rupin@gmail.com>
2013-08-29 10:52:27 +0200

    improve regex usages

--- jquery.solidify.js
+++ jquery.solidify.js
@@ -9,34 +9,27 @@
 
     $.solidify = function (rawTemplate) {
         if (!isCrawlable) {
-            var regex = /\{\{ *[#/]?solidify(-\w+)?[^\}]*\}\}/;
-            while (rawTemplate.match(regex))
-                rawTemplate = rawTemplate.replace(regex, '');
-
-            regex = /((\{ )+\{|(\} )+\})/;
-            while (rawTemplate.match(regex))
-                rawTemplate = rawTemplate.replace(regex, function (expr) {
+            rawTemplate = rawTemplate
+                .replace(/\{\{ *[#/]?solidify(-\w+)?[^\}]*\}\}/g, '')
+                .replace(/((\{ )+\{|(\} )+\})/g, function (expr) {
                     return expr.split(' ').join('');
                 });
         }
         else {
-            var inR = /\{\{ *#?(solidify(-\w+)?)[^\}]*\}\}/, s = rawTemplate;
-            while (s.match(inR)) {
-                s = s.replace(inR, function (__, name) {
-                    Handlebars.registerHelper(name, function () {
-                        var n = name.replace('solidify-', '');
-                        if (_.isFunction($.solidify.addons[n]))
-                            return $.solidify.addons[n].apply(this, arguments);
-                        var options = arguments[arguments.length - 1],
-                            args = _.map(_.toArray(arguments).slice(0, arguments.length - 1),function (e) {
-                                return '"' + e + '"';
-                            }).join(' '),
-                            isScope = _.isFunction(options.fn);
-                        return '{{' + (isScope ? '#' : '') + name + ' ' + args + '}}' + (isScope ? options.fn(this) + '{{/' + name + '}}' : '');
-                    });
-                    return '';
+            rawTemplate.replace(/\{\{ *#?(solidify(-\w+)?)[^\}]*\}\}/g, function (__, name) {
+                Handlebars.registerHelper(name, function () {
+                    var n = name.replace('solidify-', '');
+                    if (_.isFunction($.solidify.addons[n]))
+                        return $.solidify.addons[n].apply(this, arguments);
+                    var options = arguments[arguments.length - 1],
+                        args = _.map(_.toArray(arguments).slice(0, arguments.length - 1),function (e) {
+                            return '"' + e + '"';
+                        }).join(' '),
+                        isScope = _.isFunction(options.fn);
+                    return '{{' + (isScope ? '#' : '') + name + ' ' + args + '}}' + (isScope ? options.fn(this) + '{{/' + name + '}}' : '');
                 });
-            }
+                return '';
+            });
         }
         return Handlebars.compile(rawTemplate);
     };
